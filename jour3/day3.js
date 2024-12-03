f26e4397669e83fefb245d56b7dac4b661cba37b
RMat <robin.matarese@student.junia.com>
2024-12-03 12:57:35 +0100

    solution p2

--- jour3/day3.js
+++ jour3/day3.js
@@ -1,10 +1,32 @@
 const fs =require('fs');
 const input=fs.readFileSync("./input.txt","utf-8");
-const regex = /mul\((\d+),(\d+)\)/g;
-sum=0;
-while ((match = regex.exec(input)) !== null) {
+const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
+let sum=0;
+for(let match of input.matchAll(regex)){
     a=parseInt(match[1],10);
     b=parseInt(match[2],10);
     sum+=a*b;
 }
 console.log(sum);
+
+// PARTIE 2
+
+//stratégie: faire un tableau avec les positions de tout les do et don't en plus d'un tableau avec les mult
+//on commence par faire le tableau des mult
+console.log("############PARTIE 2############");
+
+const regexp2 = /(?:don't|do|mul\((\d{1,3}),(\d{1,3})\))/gm;
+result=[]
+let flag=true;
+sum=0;
+
+for(let match of input.matchAll(regexp2)){
+    if(match[0]=="don't") flag=false;
+    else if(match[0]=='do') flag=true;
+    else if(flag){
+        a=parseInt(match[1],10);
+        b=parseInt(match[2],10);
+        sum+=a*b;
+    }
+}
+console.log(sum)
